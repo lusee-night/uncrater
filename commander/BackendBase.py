eb97dafe531f8bef7bef3f7f3b4179ef5f01486b
Anže Slosar <anze@bnl.gov>
2024-08-07 11:46:10 -0400

    missing files

---
+++ commander/BackendBase.py
@@ -0,0 +1,21 @@
+class BackendBase:
+
+    def __init__(self, clog, uart_log, session):
+        self.clog = clog
+        self.uart_log = uart_log
+        self.session = session
+        self.out_dir = os.path.join(session,'cdi_output')
+        self.packet_count = 0
+
+
+
+
+    def save_data(self, appid, data):
+        fname = os.path.join(self.out_dir,f"{self.packet_count:05d}_{appid:04x}.bin")        
+        f=open(fname,'wb')
+        f.write(towrite) 
+        f.close()
+        self.clog.logt(f"Stored AppID {hex(appid)} len={len(towrite)}\n")    
+        self.packet_count += 1
+        
+    
