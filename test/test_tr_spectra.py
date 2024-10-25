ee5c56bad67599ef6b4e94c7933bd5be8b81f0c5
Arnur Nigmetov <anigmetov@lbl.gov>
2024-10-24 23:56:18 -0700

    Add helper functions to Collection to be used in tests
    - Collection.has_all_products to check that all 16 products are present
    - Collection.has_all_tr_products to check that all 16 time-resolved
      spectra are present
    - Collection.all_spectra_crc_ok to check that CRC on all spectra packets
      match
    - Collection.all_tr_spectra_crc_ok to check that CRC on all TR spectra packets
      match
    - Collection.tr_spectra: list containing all time-resolved spectra
      dicts
    - Collection.heartbeat_packets: list with all hearbeat packets
    - Collection.housekeeping_packets: list with all housekeeping packets
    - Collection.heartbeat_counter_ok: to check that there are no gaps in
      packet_count of heartbeat packets.
    - Collection.heartbeat_max_dt: return maximal time delta between
      heartbeat packets.

--- test/test_tr_spectra.py
+++ test/test_tr_spectra.py
@@ -1,28 +1,22 @@
 import os
 import sys
 import matplotlib.pyplot as plt
-import numpy as np
-import scipy as sp
-import typing
 
 from icecream import ic
 
-sys.path.append('.')
-sys.path.append('./scripter/')
-sys.path.append('./commander/')
+sys.path.append(".")
+sys.path.append("./scripter/")
+sys.path.append("./commander/")
 
-if os.environ.get('CORELOOP_DIR') is not None:
-    sys.path.append(os.environ.get('CORELOOP_DIR'))
+if os.environ.get("CORELOOP_DIR") is not None:
+    sys.path.append(os.environ.get("CORELOOP_DIR"))
 
 from test_base import Test
 from lusee_script import Scripter
-from collections import defaultdict
 
 from pycoreloop import appId
 
 import uncrater as uc
-
-
 
 
 class Test_TRSpectra(Test):
@@ -31,26 +25,24 @@ class Test_TRSpectra(Test):
     description = """ Check that time resolved spectra are received."""
     instructions = """ Do not need to connect anything."""
     default_options = {
-        "time": 10,
-        "navg2": 6,
+        "time": 30,
+        "navg1": 14,
+        "navg2": 3,
         "tr_start": 0,
-        "tr_stop": 16,
-        "tr_avg_shift": 2
+        "tr_stop": 8,
+        "tr_avg_shift": 2,
     }  ## dictinary of options for the test
     options_help = {
         "time": "Total time to run the test.",
+        "navg1": "Phase 1 (moving from FPGA) averages over 2^navg1 values.",
         "navg2": "Phase 2 (moving to TICK/TOCK) averages over 2^navg2 values.",
         "tr_start": "Start of time-resolved window.",
         "tr_stop": "End of time-resolved window.",
         "tr_avg_shift": "Average over every 2^tr_avg_shift values.",
     }  ## dictionary of help for the options
 
-
-    def correct_tr_size(self):
-        pass
-
     def generate_script(self):
-        """ Generates a script for the test """
+        """Generates a script for the test"""
         if self.time < 30:
             print("Time raised to 30 seconds.")
             self.time = 30
@@ -59,7 +51,7 @@ class Test_TRSpectra(Test):
         scripter.reset()
         scripter.wait(5)
 
-        scripter.set_Navg(Navg1=12, Navg2=self.navg2)
+        scripter.set_Navg(Navg1=self.navg1, Navg2=self.navg2)
         scripter.set_tr_start_lsb(self.tr_start)
         scripter.set_tr_stop_lsb(self.tr_stop)
         scripter.set_tr_avg_shift(self.tr_avg_shift)
@@ -71,7 +63,9 @@ class Test_TRSpectra(Test):
         return scripter
 
     def get_tr_shape(self):
-        return (self.tr_stop - self.tr_start) // ( 1 << self.tr_avg_shift ), 1 << self.navg2
+        return (self.tr_stop - self.tr_start) // (
+            1 << self.tr_avg_shift
+        ), 1 << self.navg2
 
     # plot all TR spectra and return the string with includegraphics instruction
     # do not know in advance how many plots we need
@@ -89,7 +83,9 @@ class Test_TRSpectra(Test):
                 # do not plot more than 4 first bins, it'll be impossible to parse
                 for bin_idx in range(min(data.shape[0], 4)):
                     ax[x][y].plot(data[bin_idx].flatten(), label=f"Bin {bin_idx+1}")
-                ax[x][y].set_title(f"Product {product_idx+1}/16 of packet {tr_packet_idx+1}/{len(coll.tr_spectra)}")
+                ax[x][y].set_title(
+                    f"Product {product_idx+1}/16 of packet {tr_packet_idx+1}/{len(coll.tr_spectra)}"
+                )
                 ax[x][y].set_xlabel(f"Phase 1 averaging index")
                 ax[x][y].set_xticks(range(0, data[0].size, 1 << (self.navg2 - 3)))
                 # TODO: how is this called?
@@ -100,53 +96,52 @@ class Test_TRSpectra(Test):
             result += f"\n\\includegraphics*[width=\\linewidth]{{{fig_fname}}}\n"
         return result
 
-
-    def analyze(self, C, uart, commander, figures_dir):
-        """ Analyzes the results of the test.
-            Returns true if test has passed.
+    def analyze(self, coll, uart, commander, figures_dir):
+        """Analyzes the results of the test.
+        Returns true if test has passed.
         """
         self.results = {}
-        passed = True
-
-        self.results['packets_received'] = len(C.cont)
-
-        C.cut_to_hello()
-
-        self.get_versions(C)
-
-        hb_num, sp_num, tr_sp_num = 0, 0, 0
-        hb_tmin, hb_tmax, last_hb, last_hbtime = 0, 0, None, None
-        heartbeat_counter_ok = True
+        coll.cut_to_hello()
+        self.get_versions(coll)
         tr_shape_ok = 1
 
-        for packet in C.cont:
-            if type(packet) == uc.Packet_Heartbeat:
-                hb_num += 1
-            elif type(packet) == uc.Packet_Spectrum:
-                sp_num += 1
-            elif type(packet) == uc.Packet_TR_Spectrum:
-                tr_sp_num += 1
+        # we cannot reshape when reading TR spectra from disk,
+        # because we don't know parameter for correct shape, so we have to do it here
+        for i, packet in enumerate(coll.tr_spectra):
+            for j in range(16):
+                if j not in packet:
+                    continue
                 try:
-                    packet.data = packet.data.reshape(self.get_tr_shape())
+                    packet[j].data = packet[j].data.reshape(self.get_tr_shape())
                 except:
                     tr_shape_ok = 0
-                    print(f"Wrong shape of time-resolved packet: attempted to reshape: {packet.data.shape}")
+                    print(
+                        f"Wrong shape of time-resolved packet: attempted to reshape: {packet[j].data.shape}"
+                    )
                     print(f"to new shape: {self.get_tr_shape()}")
 
-        self.results['sp_num'] = sp_num
-        self.results['sp_all'] = C.has_all_products()
-        self.results['tr_sp_all'] = C.has_all_tr_products()
-        self.results['tr_sp_num'] = tr_sp_num
-        self.results['sp_crc_ok'] = C.all_spectra_crc_ok()
-        self.results['tr_sp_crc_ok'] = C.all_tr_spectra_crc_ok()
-        self.results['tr_shape_ok'] = tr_shape_ok
-        self.results['heartbeat_received'] = hb_num
-        self.results['hearbeat_count'] = int(hb_num)
-        self.results['heartbeat_not_missing'] = int(heartbeat_counter_ok & (hb_tmax < 11))
-        self.results['sp_packets_received'] = sp_num
-        self.results['tr_sp_packets_received'] = tr_sp_num
-        self.results['tr_plots_str'] = self.plot_tr_spectra(C, figures_dir)
+        self.results["packets_received"] = len(coll.cont)
+        self.results["sp_num"] = coll.num_spectra_packets()
+        self.results["sp_all"] = coll.has_all_products()
+        self.results["tr_sp_all"] = coll.has_all_tr_products()
+        self.results["tr_sp_num"] = coll.num_tr_spectra_packets()
+        self.results["sp_crc_ok"] = coll.all_spectra_crc_ok()
+        self.results["tr_sp_crc_ok"] = coll.all_tr_spectra_crc_ok()
+        self.results["tr_shape_ok"] = tr_shape_ok
+        self.results["heartbeat_received"] = coll.num_heartbeats()
+        self.results["heartbeat_not_missing"] = int(
+            coll.hearbeat_counter_ok() and coll.heartbeat_max_dt() < 11
+        )
+        self.results["sp_packets_received"] = coll.num_spectra_packets()
+        self.results["tr_sp_packets_received"] = coll.num_tr_spectra_packets()
+        self.results["tr_plots_str"] = self.plot_tr_spectra(coll, figures_dir)
 
-        passed = self.results['sp_all'] and self.results['tr_sp_all'] and self.results['sp_crc_ok'] and self.results['tr_sp_crc_ok'] and tr_shape_ok
+        passed = (
+            self.results["sp_all"]
+            and self.results["tr_sp_all"]
+            and self.results["sp_crc_ok"]
+            and self.results["tr_sp_crc_ok"]
+            and tr_shape_ok
+        )
 
-        self.results['result'] = int(passed)
+        self.results["result"] = int(passed)
--- uncrater/Collection.py
+++ uncrater/Collection.py
@@ -18,20 +18,23 @@ class Collection:
         self.time = []
         self.desc = []
         self.spectra = []
-        self.tr_spectra = []
+        tr_spectra = []
+        self.heartbeat_packets = []
+        self.housekeeping_packets = []
         flist = glob.glob(os.path.join(self.dir, "*.bin"))
-        print(f"Ananlyzing {len(flist)} files from {self.dir}.")
+        print(f"Analyzing {len(flist)} files from {self.dir}.")
         flist = sorted(flist, key=lambda x: int(x[x.rfind("/") + 1 :].split("_")[0]))
         meta_packet = None
         for i, fn in enumerate(flist):
             # print ("reading ",fn)
             appid = int(fn.replace(".bin", "").split("_")[-1], 16)
             packet = Packet(appid, blob_fn=fn)
-            if appid_is_metadata(appid):
+            if not (appid_is_spectrum(appid) or appid_is_tr_spectrum(appid)):
                 packet.read()
+            if appid_is_metadata(appid):
                 meta_packet = packet
                 self.spectra.append({"meta": packet})
-                self.tr_spectra.append({"meta": packet})
+                tr_spectra.append({"meta": packet})
 
             if appid_is_spectrum(appid):
                 if meta_packet is not None:
@@ -40,10 +43,17 @@ class Collection:
                     self.spectra[-1][appid & 0x0F] = packet
 
             if appid_is_tr_spectrum(appid):
+                # print(f"TR appid = {appid}, from file {fn},  {fn.replace(".bin", "").split("_")[-1]}")
                 if meta_packet is not None:
                     packet.set_meta(meta_packet)
                     packet.read()
-                    self.tr_spectra[-1][appid & 0x0F] = packet
+                    tr_spectra[-1][appid & 0x0F] = packet
+
+            if appid_is_heartbeat(appid):
+                self.heartbeat_packets.append(packet)
+
+            if appid_is_housekeeping(appid):
+                self.housekeeping_packets.append(packet)
 
             self.cont.append(packet)
             self.time.append(os.path.getmtime(fn))
@@ -54,6 +64,8 @@ class Collection:
                 )
             except:
                 pass
+        self.tr_spectra = [trs for trs in tr_spectra if len(trs) > 1]
+        assert all(["meta" in trs for trs in tr_spectra])
 
     def cut_to_hello(self):
         i = len(self.cont) - 1
@@ -73,6 +85,49 @@ class Collection:
     def __len__(self):
         return len(self.cont)
 
+    # return number of spectra packets received
+    def num_spectra_packets(self) -> int:
+        return len(self.spectra)
+
+    # return number of time resolved spectra packets received
+    def num_tr_spectra_packets(self) -> int:
+        return len(self.tr_spectra)
+
+    # return number of heartbeat packets received
+    def num_heartbeats(self) -> int:
+        return len(self.heartbeat_packets)
+
+    # return number of housekeeping packets received
+    def num_housekeeping_packets(self) -> int:
+        return len(self.housekeeping_packets)
+
+    # return 1, if all heartbeat packets are present (no gaps in packet_count sequence)
+    def hearbeat_counter_ok(self) -> int:
+        hb_counts = [p.packet_count for p in self.heartbeat_packets]
+        correct_hb_counts = []
+        for i, p in enumerate(self.heartbeat_packets):
+            if i == 0:
+                correct_hb_counts.append(p.packet_count)
+            else:
+                correct_hb_counts.append(correct_hb_counts[i - 1] + 1)
+
+        if hb_counts != correct_hb_counts:
+            print(f"Bad heartbeat counts! {hb_counts} != {correct_hb_counts}")
+            return 0
+        return 1
+
+    # return maximal time difference between heartbeat packets
+    def heartbeat_max_dt(self) -> int:
+        hb_times = [p.time for p in self.heartbeat_packets]
+        deltas = [t2 - t1 for t2, t1 in zip(hb_times[1:], hb_times[:-1])]
+        return max(deltas)
+
+    # return minimal time difference between heartbeat packets
+    def heartbeat_min_dt(self) -> int:
+        hb_times = [p.time for p in self.heartbeat_packets]
+        deltas = [t2 - t1 for t2, t1 in zip(hb_times[1:], hb_times[:-1])]
+        return min(deltas)
+
     def list(self):
         return "\n".join(self.desc)
 
@@ -87,7 +142,6 @@ class Collection:
         if intro:
             return self._intro(i) + self.cont[i].info()
         return self.cont[i].info()
-
 
     # bcheckmark in TeX want an int 0/1 flag, not bool
     # return 1, if all 16 products are present
@@ -122,7 +176,9 @@ class Collection:
         for i, trs in enumerate(self.tr_spectra):
             for k in range(16):
                 if k in trs and trs[k].error_crc_mismatch:
-                    print(f"Bad CRC in product {k} in TR spectra {i}.")
+                    print(
+                        f"Bad CRC in product {k} in TR spectra {i}, incorrect CRC: {trs[k].crc}."
+                    )
                     return 0
         return 1
 
