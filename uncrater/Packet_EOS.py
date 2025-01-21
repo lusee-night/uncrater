0f870cf5b8691469312d4a06c075038c6b8ffc64
Anže Slosar <anze@bnl.gov>
2025-01-20 20:38:10 -0500

    EOS packet type

---
+++ uncrater/Packet_EOS.py
@@ -0,0 +1,24 @@
+from .PacketBase import PacketBase, pystruct
+from .utils import Time2Time
+import struct
+
+class Packet_EOS(PacketBase):
+    @property
+    def desc(self):
+        return  "Goodbye!!"
+
+    def _read(self):
+        if self._is_read:
+            return
+        super()._read()
+        self.copy_attrs(pystruct.end_of_sequence.from_buffer_copy(self._blob))
+        self._is_read = True
+        
+    def info (self):
+        self._read()
+        
+        desc = "End of Sequence Packet\n"
+        desc += f"Unique packet ID : {self.unique_packet_id}\n"
+        desc += f"EOS arg          : {self.eos_arg }\n"         
+        return desc
+    
